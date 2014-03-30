cc80ce8f28a361833b18ffa56e47d5a8d796f908
Mike Letscher <mike@3cogs.com>
2014-03-30 15:42:35 -0400

    Added more restaurants

--- out/index.js
+++ out/index.js
@@ -158,6 +158,16 @@ module.exports = (function () {
 				name: 'Chipotle Mexican Grill',
 				picture_url: 'chipotle.png',
 				description: 'It\'s not really Mexican food.'
+			},
+			244: {
+				name: 'Jimmy Johns',
+				picture_url: 'jimmyjohns.png',
+				description: 'The best gormet sandwiches.'
+			},
+			394: {
+				name: 'Dominoe\'s Pizza',
+				picture_url: 'dominoes.png',
+				description: 'The best pizza in the world.'
 			}
 		}
 	};
